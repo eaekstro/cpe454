438dcdeb0331799b5015364f8bb88a33d0fce858
Eric Ekstrom <eaekstro@calpoly.edu>
2019-09-27 17:30:22 -0700

    mmap and elf headers are parsed and printed, showing section names for confirmation. Next is page allocator.

--- src/arch/x86_64/memman.c
+++ src/arch/x86_64/memman.c
@@ -9,6 +9,8 @@
 #include "memman.h"
 
 extern int printk(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
+static void load_mmap(multiboot_tag *tag);
+static void load_elf(multiboot_tag *tag);
 
 void parse_multiboot(unsigned long addr) {
    multiboot_tag *tag;
@@ -21,8 +23,55 @@ void parse_multiboot(unsigned long addr) {
    for (tag = (multiboot_tag *) (addr + 8); tag->type != 0 && tag->size != 8;
         tag = (multiboot_tag *) ((uint8_t *) tag + ((tag->size + 7) & ~7))) {
       printk("TAG TYPE: %d\n",  tag->type);
-      if (tag->type == 2)
-         printk("BOOTLOADER: %s\n", (uint8_t *) tag + 8);
+      switch (tag->type) {
+         case MULTIBOOT_TYPE_BOOTCMD:
+            printk("BOOTLOADER CMD:::  %s\n\n", ((multiboot_string *) tag)->str);
+            break;
+         case MULTIBOOT_TYPE_BOOTNAME:
+            printk("BOOTLOADER NAME::: %s\n\n", ((multiboot_string *) tag)->str);
+            break;
+         case MULTIBOOT_TYPE_MMAP:
+            printk("MMAP:::\n");
+            load_mmap(tag);
+            break;
+         case MULTIBOOT_TYPE_ELF:
+            printk("ELF:::\n");
+            load_elf(tag);
+            break;
+      }
    }
 }
 
+static void load_mmap(multiboot_tag *tag) {
+   multiboot_mmap *entry;
+   int entry_idx = 1;
+   for (entry = (multiboot_mmap *) ((multiboot_tag_mmap *) tag)->entries;
+        ((uint64_t) entry - (uint64_t) tag) < tag->size;
+        entry = (multiboot_mmap *) ((uint64_t) entry 
+           + ((multiboot_tag_mmap *) tag)->entry_size)) {
+      printk("   ENTRY %d\n", entry_idx); 
+      printk("   ADDRESS: %lx\n" , entry->addr);
+      printk("   LENGTH:  %lx\n" , entry->len );
+      printk("   TYPE:    %x\n\n", entry->type);
+      entry_idx++;
+   }
+}
+
+static void load_elf(multiboot_tag *tag) {
+   multiboot_tag_elf *etag = (multiboot_tag_elf *) tag;
+   multiboot_elf *entry, *init_entry = etag->entries;
+   int entry_idx = 1;
+   for (entry = (multiboot_elf *)  etag->entries;
+        entry_idx <=  etag->num_entries;
+        entry = (multiboot_elf *) ((uint64_t) entry + etag->entry_size)) {
+      printk("   ENTRY %d\n", entry_idx); 
+      printk("   NAME:  %s\n",  
+         &((char *) init_entry[etag->string_table_idx].seg_addr)[entry->name_idx] );
+      printk("   TYPE:  %x\n",  entry->type );
+      printk("   FLAGS: %lx\n", entry->flags);
+      printk("   SEG ADDRESS: %lx\n",   entry->seg_addr);
+      printk("   SEG OFFSET:  %lx\n",   entry->seg_offset);
+      printk("   SEG SIZE:    %lx\n\n", entry->seg_size);
+      entry_idx++;
+   }
+}
--- src/arch/x86_64/memman.h
+++ src/arch/x86_64/memman.h
@@ -13,11 +13,62 @@
 
    #define MULTIBOOT_MAGIC 0x36d76289
 
-   typedef struct mult_tag {
+   #define MULTIBOOT_TYPE_END       0
+   #define MULTIBOOT_TYPE_BOOTCMD   1
+   #define MULTIBOOT_TYPE_BOOTNAME  2
+   #define MULTIBOOT_TYPE_BASICMEM  4
+   #define MULTIBOOT_TYPE_BIOSDEV   5
+   #define MULTIBOOT_TYPE_MMAP      6
+   #define MULTIBOOT_TYPE_ELF       9
+
+   typedef struct __attribute__ ((__packed__)) mult_tag {
       uint32_t type;
       uint32_t size;
    } multiboot_tag;
 
-   void parse_multiboot(unsigned long magic, unsigned long addr);
+   typedef struct __attribute__ ((__packed__)) mult_string {
+      uint32_t type;
+      uint32_t size;
+      char str[0];
+   } multiboot_string;
+
+   typedef struct __attribute__ ((__packed__)) mult_tag_mmap {
+      uint32_t type;
+      uint32_t size;
+      uint32_t entry_size;
+      uint32_t entry_version;
+      uint8_t  entries[0];
+   } multiboot_tag_mmap;
+   
+   typedef struct __attribute__ ((__packed__)) mult_mmap {
+      uint64_t addr;
+      uint64_t len;
+      uint32_t type;
+      uint32_t zero;
+   } multiboot_mmap;
+
+   typedef struct __attribute__ ((__packed__)) mult_tag_elf {
+      uint32_t type;
+      uint32_t size;
+      uint32_t num_entries;
+      uint32_t entry_size;
+      uint32_t string_table_idx;
+      uint8_t  entries[0];
+   } multiboot_tag_elf;
+
+   typedef struct __attribute__ ((__packed__)) mult_elf {
+      uint32_t name_idx;
+      uint32_t type;
+      uint64_t flags;
+      uint64_t seg_addr;
+      uint64_t seg_offset;
+      uint64_t seg_size;
+      uint32_t idx_link;
+      uint32_t extra_info;
+      uint64_t addr_align;
+      uint64_t iff_section;
+   } multiboot_elf;
+
+   void parse_multiboot(unsigned long addr);
 
 #endif
